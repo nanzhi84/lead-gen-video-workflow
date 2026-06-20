86f9f38a2acb2634fb51297f0345a9d870953bd4
Nanzhi <zyy2740738166@gmail.com>
2026-06-20 13:42:21 +0800

    chore(api): refresh generated OpenAPI schema

--- apps/web/src/api/schema.d.ts
+++ apps/web/src/api/schema.d.ts
@@ -8336,6 +8336,10 @@ export interface components {
             msg: string;
             /** Error Type */
             type: string;
+            /** Input */
+            input?: unknown;
+            /** Context */
+            ctx?: Record<string, never>;
         };
         /** VideoVersion */
         VideoVersion: {
